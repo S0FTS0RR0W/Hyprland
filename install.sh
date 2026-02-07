8ad09bad4906decdef502867a53b6431d371a26d
Charlie <chrlsbrwn4gt@gmail.com>
2026-02-07 22:26:22 +0000

    Add hyprlock and hypridle to package installation

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 sudo cp -r * ~/.config/
 
-sudo pacman -S kitty hyprland swww rofi brightnessctl nwg-displays fastfetch btop waybar cava thunar
+sudo pacman -S kitty hyprland hyprlock hypridle swww rofi brightnessctl nwg-displays fastfetch btop waybar cava thunar
