800a3a36205746bfb08be0ed4118aa0f8970b2fd
S0FTS0RR0W <charlie@s0fts0rr0w.com>
2026-02-24 16:02:08 +0000

    Fix: update install.sh to chown ~/.config folder

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ sudo cp -r * ~/.config/
 sudo pacman -S kitty hyprland hyprlock hypridle swww rofi brightnessctl nwg-displays fastfetch btop waybar cava thunar
 
 swww img ~/.config/background
+
+sudo chown -R $USER ~/.config
